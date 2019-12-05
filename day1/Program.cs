e04257bf4963d6f41cebf859755884edb9f4358a
Ammar Haider <ammar.haider@bjss,.com>
2019-12-05 12:59:38 +0000

    day 1

---
+++ day1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = File.ReadAllLines("input.txt")
+                .Select(long.Parse)
+                .Select(num => (num / 3) - 2)
+                .Sum();
+
+            Console.WriteLine(result);
+        }
+    }
+}
